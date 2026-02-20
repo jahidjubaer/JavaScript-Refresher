c04ee458f3c42604daacde039a63fd2c431ce61c
Jahid Hasan <jahidjubaer17@gmail.com>
2026-02-20 17:03:54 +0600

    object Tricks

---
+++ objectTricks.js
@@ -0,0 +1,15 @@
+
+const strongZone = "bowling";
+
+const player = {
+  name: "Jahid",
+  position: "open",
+  price: 100,
+  strongZone, //just put the variable name ;
+};
+
+
+// if we what a entity form a variable value;
+player[strongZone] = "left hand";
+
+console.log(player);
